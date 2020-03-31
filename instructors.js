2aa0d3d9872c71716b76e6e0845160564d6db0fb
Steven-exe <estevansantosaquino@hotmail.com>
2020-03-31 14:49:44 -0300

    update show

--- instructors.js
+++ instructors.js
@@ -1,3 +1,25 @@
+const fs = require('fs')
+const data = require("./data.json")
+
+exports.show = function(req, res){
+
+    const {id} = req.params
+
+    const foundinstruc = data.instructors.find(function(instructors){
+        return instructors.id ==  id
+    })
+
+    if (!foundinstruc) return res.send("Instructor not Found!")
+
+    const instruc = {
+        ...foundinstruc,
+        age: "",
+        services: foundinstruc.services.split(","),
+        created_at: "",
+    }
+
+    res.render("instructors/show", {instructors: instruc})
+}
 // create
 exports.post = function(req, res) {
         const keys = Object.keys(req.body)
@@ -7,6 +29,30 @@ exports.post = function(req, res) {
                 return res.send("Please, fill all the fields")
             }
         }
+        
+        let {avatar_url, name, birth, gender, services} = req.body
+        
+        birth= Date.parse(birth);
+        const criado= Date.now();
+        const id = Number(data.instructors.length + 1)
+
+
+        
+        data.instructors.push({
+            id,
+            avatar_url,
+            name,
+            birth,
+            gender,
+            services, 
+            criado
+        })
+
+        fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+            if (err) return res.send("Write file error!")
+
+            return res.redirect("/instructors/create")
+        })
     
-        return res.send(req.body)
+        //return res.send(req.body)
     }
